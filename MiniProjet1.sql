55c9e2907492ba26383deac1ba7d93a80eba9940
Mohamed Amine BAHASSOU <146652318+Medamine-Bahassou@users.noreply.github.com>
2024-01-12 23:43:32 +0100

    Create MiniProjet1.sql

---
+++ MiniProjet1.sql
@@ -0,0 +1,12 @@
+ 
+# REGLE DE GESTION
+
+1.	Chaque emprunteur est autorisé à emprunter plusieurs exemplaires.
+2.	Chaque exemplaire peut être emprunté par plusieurs emprunteurs.
+3.	Un auteur a la possibilité d'écrire plusieurs livres.
+4.	Un livre peut être rédigé par plusieurs auteurs.
+5.	Chaque livre est associé à plusieurs exemplaires.
+6.	Chaque exemplaire provient d'un unique livre.
+7.	Un livre a été édité par un seul éditeur.
+8.	Un éditeur est en mesure d'éditer plusieurs livres.
+
